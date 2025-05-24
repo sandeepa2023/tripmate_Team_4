2c51117ff4f868e0402030fedd77ea748225d4c5
M.K. Akindu Kalhan <kalhanmka.22@uom.lk>
2025-05-24 12:02:55 +0530

    resotred the backend files

---
+++ backend/OAuth2/SpringOAuth2/src/main/java/com/tripmate/SpringOAuth2/services/MyUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.tripmate.SpringOAuth2.services;
+
+
+
+import com.tripmate.SpringOAuth2.models.UserPrincipal;
+import com.tripmate.SpringOAuth2.models.Users;
+import com.tripmate.SpringOAuth2.repositories.UserRepo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService{
+
+    @Autowired
+    private UserRepo repo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Users user = repo.findByUsername(username);
+
+
+        if (user==null){
+            System.out.println("User Not Found.");
+            throw new UsernameNotFoundException("User Not Found.");
+        }
+        return new UserPrincipal(user);
+    }
+    
+}
